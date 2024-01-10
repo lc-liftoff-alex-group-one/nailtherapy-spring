94b56aae273534399e155f6cfdc89a11fb30e672
Terri Penn <coderterri@protonmail.com>
2024-01-10 11:58:41 -0600

    Initialize project with Spring Initializr

---
+++ src/main/java/com/theone/nailtherapyspring/NailtherapySpringApplication.java
@@ -0,0 +1,13 @@
+package com.theone.nailtherapyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailtherapySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailtherapySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theone/nailtherapyspring/NailtherapySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theone.nailtherapyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailtherapySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
