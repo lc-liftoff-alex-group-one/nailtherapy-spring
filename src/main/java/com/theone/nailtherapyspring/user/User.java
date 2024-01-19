3a3140a86de19321450ad9a661e96a1718402dad
Terri Penn <coderterri@protonmail.com>
2024-01-19 11:38:45 -0600

    add more properties and related methods to User model

--- src/main/java/com/theone/nailtherapyspring/user/User.java
+++ src/main/java/com/theone/nailtherapyspring/user/User.java
@@ -1,34 +1,51 @@
 package com.theone.nailtherapyspring.user;
 
 import com.theone.nailtherapyspring.models.AbstractEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.HashSet;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = { "emailAddress" })})
 public class User extends AbstractEntity {
-    @NotNull
+    @NotBlank
+    @Size(max = 32)
     private String firstName;
 
-    @NotNull
+    @NotBlank
+    @Size(max = 32)
     private String lastName;
 
+    @NaturalId
+    @NotBlank
+    @Size(max = 64)
     @Email
     private String emailAddress;
 
+    @Size(max = 16)
     private String phoneNumber;
 
+    @NotBlank
+    @Size(max = 100)
+    private String passcode;
+
+    @ManyToMany
+    @JoinTable(name = "roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
 
-    public User(String firstName, String lastName, String emailAddress, String phoneNumber) {
+    public User(String firstName, String lastName, String emailAddress, String phoneNumber, String passcode) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.emailAddress = emailAddress;
         this.phoneNumber = phoneNumber;
+        this.passcode = passcode;
     }
 
     public String getFirstName() {
@@ -61,5 +78,21 @@ public class User extends AbstractEntity {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getPasscode() {
+        return passcode;
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
