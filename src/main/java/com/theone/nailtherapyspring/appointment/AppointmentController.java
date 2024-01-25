3f6f8a857a9883994a1412503eb8223892983603
Lavender <lavendersanders88@yahoo.com>
2024-01-25 11:38:27 -0600

    created appointment controller

--- src/main/java/com/theone/nailtherapyspring/appointment/AppointmentController.java
+++ src/main/java/com/theone/nailtherapyspring/appointment/AppointmentController.java
@@ -1,19 +1,55 @@
-package com.theone.nailtherapyspring.scheduling;
+package com.theone.nailtherapyspring.appointment;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("schedule")
+@RequestMapping("appointments")
 public class AppointmentController {
 
     @Autowired
     private AppointmentRespository appointmentRespository;
 
     @GetMapping
-    public String displayAppointments(){
-
+    public String displayAppointments(Model model){
+        model.addAttribute("title", "Appointments");
+        model.addAttribute("appointments", appointmentRespository.findAll());
+        return "appointments/index";
     }
+
+    @PostMapping("book")
+    public String bookAppointment(@ModelAttribute @Valid Appointment newAppointment, Errors errors, Model model){
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Book Appointment");
+            return "appointments/details";
+        }
+        appointmentRespository.save(newAppointment);
+        return "redirect:/index";
+    }
+
+    @GetMapping("cancel")
+    public String cancelAppointment(Model model){
+      model.addAttribute("title", "Cancel Appointment");
+      model.addAttribute("appointments", appointmentRespository.findAll());
+      return "appointments/cancel";
+    }
+
+    @PostMapping("delete")
+    public String processCancellation(int[] appointmentIds){
+        if (appointmentIds != null){
+            for (int id : appointmentIds){
+                appointmentRespository.deleteById(id);
+            }
+        }
+        return "redirect:/appointments";
+    }
+
 }
